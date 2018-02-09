d9ebd71ac0b40fe551190ad29cc5f657e9a98471
mbeckerle <mbeckerle@tresys.com>
2018-02-09 10:31:42 -0500

    Convert to apache naming and package names.
    Update eclipse support.
    
    Change sbt version to 1.0.4.

--- src/test/scala/com/tresys/pcap/TestPCAP.scala
+++ src/test/scala/com/tresys/pcap/TestPCAP.scala
@@ -33,7 +33,7 @@
 package com.tresys.pcap
 
 import org.junit.Test
-import edu.illinois.ncsa.daffodil.tdml.Runner
+import org.apache.daffodil.tdml.Runner
 import org.junit.AfterClass
 
 object TestPCAP {
